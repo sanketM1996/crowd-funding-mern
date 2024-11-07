87e021e87b925369d2b482d97f6ec5b5fce479ec
sanketM1996 <sanketmahajan2496@gmail.com>
2024-11-07 19:21:40 +0530

    make changes in index

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,7 @@ function App() {
   return (
     <>
  
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       
     
     </>
   )
